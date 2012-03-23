2698361e22242f1bbed321089667f7ebfca46c63
Marilena Xirogianni <maluxir@gmail.com>
2012-03-23 21:13:10 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NocTicket::Application.config.secret_token = '1e7bbe9c5ea0eab533767adaf2176957b9f17c4caa7d666df99054ed9445ab3381b85e9528a1eba5e6be5178396812611b4f772b64beb8817bd8972b03840be2'
