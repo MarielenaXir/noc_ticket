b9b888dcdf4924db7557b7b0813887a48c1c0f1a
Marilena Xirogianni <maluxir@gmail.com>
2012-03-27 22:52:29 +0300

    User can add to ticket history

---
+++ app/controllers/histories_controller.rb
@@ -0,0 +1,18 @@
+class HistoriesController < ApplicationController
+  
+  # TODO: REFACTOR create a method on ticket
+  #       @ticket.create_user_history(params[:history], current_user)
+  def create
+    # Find the ticket throught User
+    @ticket  = current_user.tickets.find_by_id(params['ticket_id'])
+    # Create a new history for the ticket
+    @history = @ticket.histories.create(params[:history])
+    # Update ticket parameters
+    @history.user  = current_user
+    @history.state = @ticket.state
+    @history.auto  = false
+    @history.save
+    # Redirect to ticket show page
+    redirect_to ticket_path(@ticket)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 NocTicket::Application.routes.draw do
-  get "home/index"
 
   devise_for :users
+  
   resources :gizmos
   resources :profile, :only => [:show]
   resources :work,    :only => [:show]
-  resources :tickets, :only => [:new, :create, :show]
-
+  
+  resources :tickets, :only => [:new, :create, :show] do
+    resources :histories, :only => [:create]
+  end
+  
+  get "home/index"
   root :to => 'home#index'
 end
