6309ce7e9e78d0eab5cc901f2bda4a0c8e48fb83
Marilena Xirogianni <maluxir@gmail.com>
2012-03-29 19:03:28 +0300

    Fix home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+  # Code from http://ruby.zigzo.com/2011/10/02/flash-messages-twitters-bootstrap-css-framework/
+  def twitterized_type(type)
+    case type
+      when :alert
+        "warning"
+      when :error
+        "error"
+      when :notice
+        "info"
+      when :success
+        "success"
+      else
+        type.to_s
+    end
+  end
+
+
+  # Get the after signup path
+  def get_home_path
+    if current_user.nil?
+      return "/"
+    end
+    if current_user.is_user?
+      profile_path(current_user)
+    else
+      todo_path(current_user)
+    end
+  end
+
 end
