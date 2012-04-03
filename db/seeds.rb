f15d3a0be78a49709744d550f0cff33f5abd2fc9
Xirogianni Marilena <maluxir@gmail.com>
2012-04-03 07:19:37 +0300

    fix views again

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,20 @@ User.delete_all
 Ticket.delete_all
 History.delete_all
 
-vurkas = User.create({email: "vurkas@teiher.gr", password: "123qwe", password_confirmation: "123qwe", role: "admin"}, :as => :admin)
-malu   = User.create({email: "malu@teiher.gr",  password: "123qwe",  password_confirmation: "123qwe", role: "tech"},  :as => :admin)
-melina = User.create({email: "melina@teiher.gr",  password: "123qwe",  password_confirmation: "123qwe", role: "tech"},  :as => :admin)
+basilakis = User.create({email: "K.Vassilakis@teicrete.gr", password: "1234qwer", password_confirmation: "1234qwer", role: "admin"}, :as => :admin)
+vurkas = User.create({email: "vurkas@teicrete.gr", password: "123qwe", password_confirmation: "123qwe", role: "tech"}, :as => :admin)
+malu   = User.create({email: "malu@teicrete.gr",  password: "123qwe",  password_confirmation: "123qwe", role: "tech"},  :as => :admin)
+melina = User.create({email: "melina@teicrete.gr",  password: "123qwe",  password_confirmation: "123qwe", role: "tech"},  :as => :admin)
 
-manos  = User.create email: "manos@teiher.gr",  password: "123qwe",  password_confirmation: "123qwe"
-nikos  = User.create email: "nikos@teiher.gr",  password: "123qwe",  password_confirmation: "123qwe"
+manos  = User.create email: "manos@teicrete.gr",  password: "123qwe",  password_confirmation: "123qwe"
+nikos  = User.create email: "nikos@teicrete.gr",  password: "123qwe",  password_confirmation: "123qwe"
 
 
 10.times do |i|
-  u = User.create email: "user#{i}@teiher.gr",  password: "123qwe",  password_confirmation: "123qwe"  
+  u = User.create email: "user#{i}@teicrete.gr",  password: "123qwe",  password_confirmation: "123qwe"  
   rand(4).times do |j|
     t = u.create_ticket(name: "A ticket #{j} by user user#{i}", description: Faker::Lorem.paragraph(3))
     tech = [malu, melina,vurkas].shuffle.first
-    t.assign_to(tech,vurkas) unless rand(10) > 7
+    t.assign_to(tech,basilakis) unless rand(10) > 7
   end
 end
